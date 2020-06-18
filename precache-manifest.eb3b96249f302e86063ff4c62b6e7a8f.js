a19d822a4d94e6c04fe94ae64f48d033588fdf82
NiroNisha698 <nironisha0606@gmail.com>
2020-06-18 21:13:17 +0530

    Updates

---
+++ precache-manifest.eb3b96249f302e86063ff4c62b6e7a8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6d4625b14053cc7569955de8237272",
+    "url": "/React-todo-list-demo/index.html"
+  },
+  {
+    "revision": "72afec38a2a4bae8c6af",
+    "url": "/React-todo-list-demo/static/css/main.c0bb8111.chunk.css"
+  },
+  {
+    "revision": "63aa161409598bcfc3d5",
+    "url": "/React-todo-list-demo/static/js/2.9c9e0fce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo-list-demo/static/js/2.9c9e0fce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72afec38a2a4bae8c6af",
+    "url": "/React-todo-list-demo/static/js/main.c249d800.chunk.js"
+  },
+  {
+    "revision": "a85c5a4af2f1be974b4a",
+    "url": "/React-todo-list-demo/static/js/runtime-main.4017a4c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-todo-list-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
